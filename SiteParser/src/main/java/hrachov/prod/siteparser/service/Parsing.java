e728a17c43006ccba3dd745084d87adc4b1bc03b
DmitriyGrachev <90601860+DmitriyGrachev@users.noreply.github.com>
2025-03-08 11:37:07 +0200

    Delete SiteParser directory

--- SiteParser/src/main/java/hrachov/prod/siteparser/service/Parsing.java
+++
@@ -1,9 +0,0 @@
-package hrachov.prod.siteparser.service;
-
-import hrachov.prod.siteparser.model.Product;
-
-import java.util.List;
-
-public interface Parsing {
-    public List<Product> getProductsFromHtml(String nameProduct);
-}
